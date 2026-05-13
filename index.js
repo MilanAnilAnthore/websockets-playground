a615f69cc799c9e054d6d95315a6d085b60ed459
MilanAnilAnthore <157392523+MilanAnilAnthore@users.noreply.github.com>
2026-05-12 22:31:45 -0400

    Add game creation UI and WS create handling
    Client: add a simple UI (New Game button) and wire WebSocket messages to set clientId on connect and send a 'create' request when the button is clicked; handle server 'create' responses to store gameId and ball count. Server: serve index.html from Express (listening on 9091), import crypto, introduce a games store, generate game IDs with crypto.randomUUID(), and handle incoming 'create' requests by creating a game (20 balls) and sending the created game back to the requesting client. Also fix request.accept to use request.origin and adjust some log messages.

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const http = require("http");
-
+const { json } = require("stream/consumers");
+const crypto = require("crypto");
 const express = require("express");
-const app = express();
+const webSocketServer = require("websocket").server;
 
+const app = express();
+app.listen(9091, ()=> console.log("Listening on server 9091"));
 app.get("/", (req,res)=>{
-    res.sendFile()
+    res.sendFile(__dirname + "/index.html")
 })
 
-const { json } = require("stream/consumers");
-const webSocketServer = require("websocket").server;
+
 const httpServer = http.createServer();
-httpServer.listen(9090, ()=> console.log("Listening on server 9090"));
+httpServer.listen(9090, ()=> console.log("Listening on port 9090"));
 
 const clients = {};
+const games = {};
 
 const wsServer = new webSocketServer({
     "httpServer" : httpServer
@@ -21,7 +24,7 @@ const wsServer = new webSocketServer({
 wsServer.on("request", request =>{
 
     // connection
-    const connection = request.accept(null, origin);
+    const connection = request.accept(null, request.origin);
 
     connection.on("open", ()=> console.log("Connection open"));
     connection.on("close", ()=> console.log("Connection closed"));
@@ -29,7 +32,26 @@ wsServer.on("request", request =>{
     connection.on("message", message =>{
 
         const result = JSON.parse(message.utf8Data);
-        console.log(result);
+        
+        // I have received a message from client
+        //a user want to create a new gane
+        if(result.method === "create"){
+            const clientId = result.clientId;
+            const gameId = crypto.randomUUID();
+
+            games[gameId]={
+                "id": gameId,
+                "balls": 20
+            }
+
+            const payLoad = {
+                "method": "create",
+                "game": games[gameId]
+            }
+
+            const con = clients[clientId].connection;
+            con.send(JSON.stringify(payLoad))
+        }
     })
 
     const clientId = crypto.randomUUID();
